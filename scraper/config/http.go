8fb2b83ed792d67c88f54e2d07a660b4e6b96209
Jonah Lewis <lewisj41@unlv.nevada.edu>
2025-04-05 20:58:49 -0700

    refactor http to be in its own file for config

---
+++ scraper/config/http.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HandleConfig(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		HandleGetConfig(w, r)
+	} else if r.Method == "POST" {
+		HandleSetConfig(w, r)
+	}
+}
+
+func HandleGetConfig(w http.ResponseWriter, r *http.Request) {
+	//encode the structure to json
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(&_config)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+func HandleSetConfig(w http.ResponseWriter, r *http.Request) {
+	//decode the passed json
+	decoder := json.NewDecoder(r.Body)
+	//TODO make this thread safe
+	err := decoder.Decode(&_config)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
