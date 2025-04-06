a33bb51c3f638b19b01ac909e4ff03250bad45cf
melon <90787267+43104@users.noreply.github.com>
2025-04-05 22:11:44 -0700

    add response

--- scraper/app/response/response.go
+++ scraper/app/response/response.go
@@ -1,6 +1,12 @@
 package response
 
 type response struct {
-	ID      int64 `json:"id"`
-	AnimeID int64 `json:"anime_id"`
+	ID         int64  `json:"id"`
+	AnimeID    int64  `json:"anime_id"`
+	AnimeArt   string `json:"anime_image"`
+	Acimage    string `json:"character_image"`
+	AnimeTitle string `json:"origin"`
+	Cgender    string `json:"gender"`
+	Cname      string `json:"name"`
+	Cdesc      string `json:"desc"`
 }
