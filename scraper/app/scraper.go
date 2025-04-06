ee2c229c096548f490619ec021d1fbc11996a659
Jonah Lewis <lewisj41@unlv.nevada.edu>
2025-04-05 22:31:21 -0700

    use a docker base image that includes ssl certificates

--- scraper/app/scraper.go
+++ scraper/app/scraper.go
@@ -1,16 +1,17 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	config "github.com/jonahlewis4/Anime-Stock-Exchange/scraper/app/config"
+	api "github.com/jonahlewis4/Anime-Stock-Exchange/scraper/app/response"
+	"net/http"
+	"strings"
 )
 
 type Scraper struct {
 	Config config.Config
 	server Restserver
-}
-
-type Response struct {
 }
 
 func (s *Scraper) Init() {
@@ -22,4 +23,30 @@ func (s *Scraper) Init() {
 func (s *Scraper) Scrape() {
 	fmt.Println("Scraping...")
 	//TODO get the info
+
+	request, err := http.NewRequest("GET", "https://www.animecharactersdatabase.com/api_series_characters.php?character_id=56145", strings.NewReader(""))
+	if err != nil {
+		fmt.Println("error creating request", err)
+		return
+	}
+
+	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		fmt.Println("Error sending request", err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Error hitting 3rd party character api: ", response.Status)
+	}
+
+	apiResponse := &api.Response{}
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(apiResponse)
+	if err != nil {
+		fmt.Println("Error decoding api response", err)
+	}
+
+	fmt.Println(apiResponse)
+
 }
--- scraper/app/server.go
+++ scraper/app/server.go
@@ -11,7 +11,7 @@ func (server *Restserver) Init(scraper *Scraper) {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/config", scraper.Config.Handle)
-	mux.HandleFunc("/activate", scraper.HandleRun)
+	mux.HandleFunc("/scrape", scraper.HandleRun)
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println(err)
